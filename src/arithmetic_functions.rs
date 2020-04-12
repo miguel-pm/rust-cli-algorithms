64c6865855561ae9b458edd13d2e483ffcb7a497
MiguelPM <miguelpm@protonmail.com>
2020-04-12 12:55:03 +0200

    refactor: largest number function

--- src/arithmetic_functions.rs
+++ src/arithmetic_functions.rs
@@ -14,7 +14,7 @@ pub fn avg (list: &[i128]) -> i128 {
     sum_list(list) / list.len() as i128
 }
 
-// Returns the largest element in a list
+/// Returns the largest element in a list
 pub fn largest<T: PartialOrd + Copy>(list: &Vec<T>) -> Option<T> {
     let length = list.len();
     if length == 0 {
@@ -27,12 +27,12 @@ pub fn largest<T: PartialOrd + Copy>(list: &Vec<T>) -> Option<T> {
     let second = list.get(1).unwrap();
 
     let largest_num = if first >= second {
-        first
+        *first
     } else {
-        second
+        *second
     };
-    if length == 2 { return Some(*largest_num); }
+    if length == 2 { return Some(largest_num); }
     let mut new_list = Vec::from(&list[2..]);
-    new_list.push(*largest_num);
+    new_list.push(largest_num);
     largest(&new_list)
 }
